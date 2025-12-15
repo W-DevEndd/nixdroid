d1d862bf3f92bd81a27d1279ab30b94597a542e2
W. Dev Endd <W-DevEndd@github.com>
2025-12-15 23:32:35 +0000

    initial

---
+++ home/manager.nix
@@ -0,0 +1,36 @@
+{ pkgs, ... }:
+
+{
+  # Read Nix-on-Droid changelog before changing this value
+  system.stateVersion = "24.05";
+
+  # insert Nix-on-Droid config
+
+  home-manager.config =
+    { pkgs, ... }:
+    {
+      # Read home-manager changelog before changing this value
+      home.stateVersion = "24.05";
+
+      programs.git = {
+        enable = true;
+        userName = "W. Dev Endd";
+        userEmail = "W-DevEndd@github.com";
+        extraConfig = {
+          init.defaultBranch = "main";
+          push.autoSetupRemote = true; # Example: automatically set upstream for pushed branches
+        };
+        aliases = {
+          ci = "commit";
+          co = "checkout";
+          s = "status";
+        };
+        lfs.enable = true;
+      };
+
+      # insert home-manager config
+    };
+
+  # or if you have a separate home.nix already present:
+  # home-manager.config = ./home.nix;
+}
