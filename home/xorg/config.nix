de25218d6c563c8ebd389f4e85cc774982cea1a2
W. Dev Endd <W-DevEndd@github.com>
2025-12-17 14:29:27 +0000

    ;-;

--- home/xorg/config.nix
+++ home/xorg/config.nix
@@ -1,7 +1,8 @@
 { pkgs, config, lib, ... }:
 
 {
-  config.home.file.".Xresources".text = "Xft.dpi: 168";
+  config.home.file.".Xresources".text = "Xft.dpi: 168
+";
 
   imports = [
     ./i3wm/config.nix
