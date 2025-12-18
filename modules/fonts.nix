cdb89026bae3a0334ecee6f2713df2b22914ee79
W-DevEndd <W-DevEndd@github.com>
2025-12-18 07:45:58 +0700

    ...

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,31 +1,30 @@
 { pkgs, ... }:
 
 {
-  fonts = {
-    packages = with pkgs; [
-      noto-fonts
-      noto-fonts-cjk-sans
-      noto-fonts-emoji
-      font-awesome
+  packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-emoji
+    font-awesome
 
-      (nerdfonts.override { 
-        fonts = [ "JetBrainsMono" ]; 
-      })
+    (nerdfonts.override { 
+      fonts = [ "JetBrainsMono" ]; 
+    })
 
-      # 25.05
-      # nerd-fonts.jetbrains-mono
-      # nerd-fonts.fira-code
-    ];
+    # 25.05
+    # nerd-fonts.jetbrains-mono
+    # nerd-fonts.fira-code
+  ];
 
-    fontconfig = {
-      enable = true;
-      defaultFonts = {
-        serif = [ "Noto Serif" ];
-        sansSerif = [ "Noto Sans" ];
-        monospace = [ "JetBrainsMono Nerd Font" ];
-      };
-    };
-
-    fontDir.enable = true;
-  };
+    # fontconfig = {
+    #   enable = true;
+    #   defaultFonts = {
+    #     serif = [ "Noto Serif" ];
+    #     sansSerif = [ "Noto Sans" ];
+    #     monospace = [ "JetBrainsMono Nerd Font" ];
+    #   };
+    # };
+    #
+    # fontDir.enable = true;
+    terminal.font = "${pkgs.nerdfonts.override { fonts = ["JetBrainsMono"]; }}/share/fonts/truetype/NerdFonts/JetBrainsMonoNerdFont-Regular.ttf";
 }
