c61bbd0ae863811ca0a111aab7d921cf3361119f
W-DevEndd <W-DevEndd@github.com>
2025-12-18 22:54:50 +0700

    detete bloatn't ware:))

--- modules/packages.nix
+++ modules/packages.nix
@@ -12,22 +12,22 @@
     # })
 
     # CLI
-    procps
-    killall
-    diffutils
-    findutils
-    utillinux
-    tzdata
-    hostname
-    man
-    gnugrep
-    gnupg
-    gnused
-    gnutar
-    bzip2
-    gzip
-    xz
-    zip
-    unzip
+    # procps
+    # killall
+    # diffutils
+    # findutils
+    # utillinux
+    # tzdata
+    # hostname
+    # man
+    # gnugrep
+    # gnupg
+    # gnused
+    # gnutar
+    # bzip2
+    # gzip
+    # xz
+    # zip
+    # unzip
   ];
 }
